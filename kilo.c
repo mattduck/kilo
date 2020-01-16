c0be53c1adbdb816cbcf2435c49c1728f10a6526
Matthew Duck <matt@mattduck.com>
2020-01-16 10:45:11 +0000

    Chapter 1
    Puts terminal in raw mode, adds the main loop to read input and quit
    on 'q'.

--- kilo.c
+++ kilo.c
@@ -1,3 +1,48 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+struct termios orig_termios;
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetatr");
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;  // 100ms
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
 int main() {
+  enableRawMode();
+
+  char c = '\0';
+  while (1) {
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);  // %d is decimal
+    } else {
+      printf("%d ('%c')\r\n", c, c);  // %c is byte
+    }
+    if (c == 'q') break;
+  }
   return 0;
 }
